db95d725b9e2f7622e6ef24b917294cf03bf2ad1
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-06-03 10:21:34 +0100

    added search icon

---
+++ components/icon/search.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <svg :width="width" :height="height" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="19.6108" cy="19.611" r="14.9809" stroke="#603F8B" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M30.0303 30.8086L35.9037 36.6667" stroke="#603F8B" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</div>
+</template>
+
+<script setup lang="ts">
+    defineProps({
+        width: {
+            type: String,
+            default: 40
+
+        },
+        height: {
+            type: String,
+            default: 40
+        }
+    })
+</script>
