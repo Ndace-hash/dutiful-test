2ae90bedef32df8b5dc2dec0917f74c270d22575
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-06-03 19:54:17 +0100

    added color to headline and short text

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="flex flex-col gap-6 md:flex-row items-center">
         <div class="max-w-[400px]" >
-            <h1 class="font-bold text-3xl mt-3 leading-10 mb-2">List and grow yur business with just few clicks</h1>
-            <p class="text-base">Eliminate manual work and save time by listing your business or service on the best business directory in Nigeria.</p>
+            <h1 class="font-bold text-3xl mt-3 leading-10 mb-2 text-dark-blue">List and grow yur business with just few clicks</h1>
+            <p class="text-base text-black">Eliminate manual work and save time by listing your business or service on the best business directory in Nigeria.</p>
         </div>
         <div class="relative flex justify-center items-end w-full">
             <div class="bg-primary absolute h-3/4 bottom-0 left-0 right-0 -z-10 rounded-xl"></div>
--- components/Reviews.vue
+++ components/Reviews.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="flex flex-col gap-6  items-center mt-6 md:flex-row-reverse md:justify-between">
         <div class="max-w-[400px]" >
-            <h1 class="font-bold text-3xl mt-3 leading-10 mb-2">Collect reviews and ratings from customers</h1>
-            <p class="text-base">Accelerate your business reputation by 90% and gain social proof through reviews and ratings requested from customers.</p>
+            <h1 class="font-bold text-3xl mt-3 leading-10 mb-2 text-dark-blue">Collect reviews and ratings from customers</h1>
+            <p class="text-base text-black">Accelerate your business reputation by 90% and gain social proof through reviews and ratings requested from customers.</p>
         </div>
         <div class="relative  max-w-[500px] justify-self-start w-full bg-[url(~/assets/images/Elipse.svg)] bg-contain bg-no-repeat bg-center min-h-[400px]">
             <img src="~/assets/images/reviews1.svg" alt="focal point: preview of app" class="absolute w-[200px] left-1/4 top-1/4 -translate-x-1/3 -translate-y-1/3 z-50">
