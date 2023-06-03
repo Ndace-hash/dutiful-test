0f5eb8d56172f7180cb75c92993bc154a1e956dc
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-06-03 10:21:08 +0100

    added headers for both desktop and mobile

---
+++ components/DesktopHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="justify-between items-center py-2 px-12 hidden md:flex">
+    <div class="w-20 ">
+      <img src="/logo.svg" alt="LOGO" class="w-full">
+    </div>
+    <div class="flex gap-4">
+      <div class="flex items-center gap-4">
+        <IconSearch width="20px" height="20px"/>
+        <ul class="flex gap-3 items-center">
+          <li class="capitalize text-black text-sm"><NuxtLink to="/">business directory</NuxtLink></li>
+          <li class="capitalize text-black text-sm"><NuxtLink to="/">features</NuxtLink></li>
+          <li class="capitalize text-black text-sm"><NuxtLink to="/">pricing</NuxtLink></li>
+          <li class="capitalize text-black text-sm"><NuxtLink to="/">blog</NuxtLink></li>
+        </ul>
+      </div>
+      <div class="flex gap-4">
+        <button class="capitalize font-semibold">login</button>
+        <button class="capitalize font-semibold border-primary border-2 px-12 py-3 text-primary text-base rounded-lg">sign up</button>
+      </div>
+    </div>
+  </header>
+</template>
+
---
+++ components/MobileHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="flex items-center justify-between px-[30px] py-4 md:hidden">
+    <div class="flex items-center justify-center w-20">
+      <img src="/logo.svg" alt="LOGO" class="w-full">
+    </div>
+    <button class="py-4 px-3 flex flex-col gap-3 items-center justify-center">
+      <span class="min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full"></span>
+      <span class="min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full"></span>
+    </button>
+  </header>
+</template>
+
