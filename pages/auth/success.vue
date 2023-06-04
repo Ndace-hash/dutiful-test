37827caa1e4af3cfab05186d1cc83fe2b02bf6a3
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-06-04 19:16:53 +0100

    success login

---
+++ pages/auth/success.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full min-h-[500px] flex items-center justify-center">
+        <h2 class="capitalize text-xl text-center font-bold text-primary flex items-center justify-center gap-2">
+            Login successful
+            <IconCheck width="40" height="40"/>
+        </h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
