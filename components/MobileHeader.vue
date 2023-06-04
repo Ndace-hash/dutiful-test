e4cf34e18b80aa8484377ce5f3171204df5ed7c8
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-06-04 23:12:10 +0100

    added side bar

--- components/MobileHeader.vue
+++ components/MobileHeader.vue
@@ -1,12 +1,19 @@
 <template>
-  <header class="flex items-center justify-between px-[30px] py-4 md:hidden z-30">
+  <header class="flex items-center justify-between px-[30px] py-4 md:hidden">
     <NuxtLink to="/" class="flex items-center justify-center w-20">
       <img src="/logo.svg" alt="LOGO" class="w-full">
     </NuxtLink>
-    <button class="py-4 px-3 flex flex-col gap-3 items-center justify-center">
-      <span class="min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full"></span>
-      <span class="min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full"></span>
+    <button class="py-4 px-3 flex flex-col gap-3 items-center justify-center z-50" @click="toggleNav">
+      <span :class="['min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full ease-in duration-200',showNav ? 'rotate-45 translate-y-2 translate-x-1' : '']"></span>
+      <span :class="['min-w-[40px] w-full min-h-[3px] bg-primary block rounded-full ease-in duration-200',showNav ? '-rotate-45 translate-x-1 -translate-y-2' : '']"></span>
     </button>
+    <Sidebar :class="['absolute ease-in duration-1000', showNav ? 'translate-x-0 block opacity-100' : 'translate-x-full hidden opacity-0']" @close="showNav = false"/>
   </header>
 </template>
 
+<script setup>
+  const showNav = ref(false)
+  const toggleNav = () => {
+    showNav.value = !showNav.value
+  }
+</script>
---
+++ components/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-black absolute inset-0 bg-opacity-50 backdrop-blur-sm flex justify-end z-[40]" @click="$emit('close')">
+        <ul class="bg-white h-full w-3/4 rounded-lg flex flex-col items-center justify-center gap-6">
+            <li class="text-xl font-semibold capitalize">
+                <NuxtLink to="/">Business directory</NuxtLink>
+            </li>
+            <li class="text-xl font-semibold capitalize">
+                <NuxtLink to="/">features</NuxtLink>
+            </li>
+            <li class="text-xl font-semibold capitalize">
+                <NuxtLink to="/">Pricing</NuxtLink>
+            </li>
+            <li class="text-xl font-semibold capitalize">
+                <NuxtLink to="/">blog</NuxtLink>
+            </li>
+            <div class="flex flex-col gap-4 items-center mt-4">
+                <NuxtLink to="/auth/login"
+                    class="capitalize font-bold text-2xl border-primary border-2 px-12 py-2  w-full text-primary text-center rounded-lg">
+                    login</NuxtLink>
+                <NuxtLink to="/auth/register"
+                    class="capitalize font-semibold border-primary bg-primary border-2 px-12 py-3 text-[#ffffff] text-2xl w-full rounded-lg">
+                    sign up</NuxtLink>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
